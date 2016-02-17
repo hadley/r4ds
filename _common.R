62752f49185c33d77cb63544284ab961edd13710
hadley <h.wickham@gmail.com>
2016-02-17 16:33:25 +1100

    Run common code at the start of every chapter

---
+++ _common.R
@@ -0,0 +1,10 @@
+set.seed(1014)
+options(digits = 3)
+
+knitr::opts_chunk$set(
+  comment = "#>",
+  collapse = TRUE,
+  cache = TRUE
+)
+
+options(dplyr.print_min = 6, dplyr.print_max = 6)
--- common.R
+++
@@ -1,7 +0,0 @@
-set.seed(1014)
-options(digits = 3)
-
-knitr::opts_chunk$set(
-  comment = "#>",
-  collapse = TRUE
-)
