11294f5d0cb66cb457b6e18e0da30a2a32c49fad
hadley <h.wickham@gmail.com>
2016-07-18 09:52:55 -0500

    Tweak figures through out book

--- _common.R
+++ _common.R
@@ -4,7 +4,12 @@ options(digits = 3)
 knitr::opts_chunk$set(
   comment = "#>",
   collapse = TRUE,
-  cache = TRUE
+  cache = TRUE,
+  out.width = "70%",
+  fig.align = 'center',
+  fig.width = 6,
+  fig.asp = 0.618,  # 1 / phi
+  fig.show = "hold"
 )
 
 options(dplyr.print_min = 6, dplyr.print_max = 6)
