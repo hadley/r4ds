642cf9f3ef027758eeff4e47ca4297f50e53b149
Hadley Wickham <h.wickham@gmail.com>
2022-02-15 11:59:19 -0600

    Polishing data transformation

--- _common.R
+++ _common.R
@@ -23,15 +23,16 @@ options(
 
 status <- function(type) {
   status <- switch(type,
-    restructuring = "undergoing heavy restructuring and may be confusing or incomplete",
-    drafting = "currently a dumping ground for ideas, and we don't recommend reading it",
+    polishing = "should be readable but is currently undergoing final polishing",
+    restructuring = "is undergoing heavy restructuring and may be confusing or incomplete",
+    drafting = "is currently a dumping ground for ideas, and we don't recommend reading it",
     stop("Invalid `type`", call. = FALSE)
   )
 
   cat(paste0(
     "::: {.rmdnote}\n",
     "You are reading the work-in-progress second edition of R for Data Science. ",
-    "This chapter is currently ", status, ". ",
+    "This chapter ", status, ". ",
     "You can find the polished first edition at <https://r4ds.had.co.nz>.\n",
     ":::\n"
   ))
