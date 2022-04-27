97b074509db6b04ffe025c9a7a7b458c904eef89
Hadley Wickham <h.wickham@gmail.com>
2022-04-27 08:01:00 -0500

    Polishing pipes now that 4.2 is out

--- _common.R
+++ _common.R
@@ -26,6 +26,7 @@ status <- function(type) {
     polishing = "should be readable but is currently undergoing final polishing",
     restructuring = "is undergoing heavy restructuring and may be confusing or incomplete",
     drafting = "is currently a dumping ground for ideas, and we don't recommend reading it",
+    complete = "is largely complete",
     stop("Invalid `type`", call. = FALSE)
   )
 
@@ -33,7 +34,7 @@ status <- function(type) {
     "::: {.rmdnote}\n",
     "You are reading the work-in-progress second edition of R for Data Science. ",
     "This chapter ", status, ". ",
-    "You can find the polished first edition at <https://r4ds.had.co.nz>.\n",
+    "You can find the complete first edition at <https://r4ds.had.co.nz>.\n",
     ":::\n"
   ))
 }
